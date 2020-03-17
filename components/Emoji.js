791640179f72a2b9e71fe0eb80d166081e220596
andykitt <andypkitt@gmail.com>
2020-03-17 08:29:15 +0000

    change emoji to class component

--- components/Emoji.js
+++ components/Emoji.js
@@ -10,20 +10,29 @@ import Image from './Image';
 
 const {width} = Dimensions.get('window');
 
-const Emoji = ({emoji, onSelect}) => {
-  return (
-    <TouchableOpacity
-      style={{width: width / 6}}
-      onPress={() => onSelect(emoji)}>
-      <View style={styles.emojiWrapper}>
-        <Image source={emoji.images[0]} style={styles.imageStyle} />
-        {emoji.skin_variations && (
-          <Text style={{fontSize: 30, position: 'absolute'}}>.</Text>
-        )}
-      </View>
-    </TouchableOpacity>
-  );
-};
+class Emoji extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.emoji.name === 'SQUARED OK') {
+      console.log(nextProps.emoji != this.props.emoji);
+    }
+    return nextProps.emoji != this.props.emoji;
+  }
+  render() {
+    const {emoji, onSelect} = this.props;
+    return (
+      <TouchableOpacity
+        style={{width: width / 6}}
+        onPress={() => onSelect(emoji)}>
+        <View style={styles.emojiWrapper}>
+          <Image source={emoji.images[0]} style={styles.imageStyle} />
+          {emoji.skin_variations && (
+            <Text style={{fontSize: 30, position: 'absolute'}}>.</Text>
+          )}
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   imageStyle: {
