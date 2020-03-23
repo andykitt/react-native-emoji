cef98055d886f6aeef00dec34518d2fac8379d3c
Andy Kitt <andypkitt@gmail.com>
2020-03-23 12:59:28 +0000

    push

--- components/Emoji.js
+++ components/Emoji.js
@@ -11,25 +11,54 @@ import Image from './Image';
 const {width} = Dimensions.get('window');
 
 class Emoji extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.props.emoji.name === 'SQUARED OK') {
-      console.log(nextProps.emoji != this.props.emoji);
+  state = {
+    tones: [],
+  };
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (this.props.emoji.name === 'SQUARED OK') {
+  //     console.log(nextProps.emoji != this.props.emoji);
+  //   }
+  //   return nextProps.emoji != this.props.emoji;
+  // }
+
+  onSelect = emoji => {
+    if (emoji.skin_variations) {
+      this.setState({tones: emoji.images});
+    } else {
+      this.props.navigation.navigate('Home');
+      this.setState({tones: []});
     }
-    return nextProps.emoji != this.props.emoji;
-  }
+  };
+
   render() {
     const {emoji, onSelect} = this.props;
+    const {tones} = this.state;
     return (
-      <TouchableOpacity
-        style={{width: width / 6}}
-        onPress={() => onSelect(emoji)}>
-        <View style={styles.emojiWrapper}>
-          <Image source={emoji.images[0]} style={styles.imageStyle} />
-          {emoji.skin_variations && (
-            <Text style={{fontSize: 30, position: 'absolute'}}>.</Text>
-          )}
-        </View>
-      </TouchableOpacity>
+      <View>
+        {tones && Boolean(tones.length) && (
+          <View style={[styles.skinEmojiWrapper, {zIndex: 100}]}>
+            {tones.slice(0, 6).map((image, i) => (
+              <TouchableOpacity key={i} onPress={() => this.onSelect(image)}>
+                <View style={styles.emojiWrapper}>
+                  <Image source={image} style={styles.imageStyle} />
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+        )}
+        <TouchableOpacity
+          style={{width: width / 6}}
+          onPress={() => this.onSelect(emoji)}>
+          <View style={styles.emojiWrapper}>
+            <Image source={emoji.images[0]} style={styles.imageStyle} />
+            {emoji.skin_variations && (
+              <Text style={{fontSize: 30, position: 'absolute', bottom: 0}}>
+                .
+              </Text>
+            )}
+          </View>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -39,10 +68,35 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: 32,
     height: 32,
+    zIndex: -1,
   },
   emojiWrapper: {
     position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 30,
+    zIndex: 1,
+  },
+  skinEmojiWrapper: {
+    width: width,
+    position: 'absolute',
+    top: -32,
+    left: 0,
+    right: 0,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    zIndex: 100,
   },
 });
 
--- components/FlatList.js
+++ components/FlatList.js
@@ -1,30 +1,61 @@
 import React from 'react';
-import {FlatList} from 'react-native';
+import {
+  FlatList,
+  Dimensions,
+  InteractionManager,
+  ActivityIndicator,
+  Animated,
+} from 'react-native';
 import Emoji from './Emoji';
 
+const {width} = Dimensions.get('window');
+
 class FlatListContainer extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.onSelect != this.props.onSelect;
+  state = {
+    opacity: 0,
+  };
+  componentDidMount() {
+    InteractionManager.runAfterInteractions(() => {
+      this.setState({opacity: 1});
+    });
   }
-  renderItem = item => <Emoji emoji={item} onSelect={this.props.onSelect} />;
+
+  renderItem = item => (
+    <Emoji
+      emoji={item}
+      onSelect={this.props.onSelect}
+      navigation={this.props.navigation}
+    />
+  );
 
   render() {
     const {emojis} = this.props;
     return (
-      <FlatList
-        keyExtractor={item => item.unified}
-        data={emojis}
-        numColumns={6}
-        windowSize={6}
-        contentContainerStyle={{
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          paddingLeft: 20,
-        }}
-        removeClippedSubviews
-        horizontal={false}
-        renderItem={({item}) => this.renderItem(item)}
-      />
+      <>
+        <FlatList
+          keyExtractor={item => item.unified}
+          data={emojis}
+          numColumns={6}
+          removeClippedSubviews
+          windowSize={2}
+          getItemLayout={(data, index) => ({
+            length: 60,
+            offset: (width / 6) * index,
+            index,
+          })}
+          windowSize={6}
+          contentContainerStyle={{
+            alignItems: 'flex-start',
+            justifyContent: 'center',
+            opacity: this.state.opacity,
+            zIndex: 1,
+          }}
+          removeClippedSubviews
+          horizontal={false}
+          renderItem={({item}) => this.renderItem(item)}
+        />
+        {!this.state.opacity && <ActivityIndicator />}
+      </>
     );
   }
 }
